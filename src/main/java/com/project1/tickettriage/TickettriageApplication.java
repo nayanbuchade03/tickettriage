e7ff782c7d4d44479f3c72faf72556e8a5a04b62
shree <buchadeshree33@gmail.com>
2026-04-23 16:41:44 +0530

    Initial Commit

---
+++ src/main/java/com/project1/tickettriage/TickettriageApplication.java
@@ -0,0 +1,13 @@
+package com.project1.tickettriage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickettriageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickettriageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project1/tickettriage/TickettriageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project1.tickettriage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickettriageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
